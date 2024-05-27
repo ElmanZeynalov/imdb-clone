122172086bd1490baf2513ee3adac14d6b26b3db
elmanzeynalov <elman.zeynalov@gmail.com>
2024-05-27 17:08:14 +0400

    creat the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div className='max-w-6xl mx-auto p-3 flex flex-col gap-4'>
+            <h1 className='text-2xl font-medium text-amber-600'>
+                About
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, aliquid aperiam asperiores
+                atque beatae debitis dolore facilis iure labore minima nostrum quasi quis rem sed temporibus ut
+                voluptate!</p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aperiam, architecto beatae culpa
+                cum ea esse ex, hic iste magni natus nemo perferendis quo ratione sequi soluta. Accusamus asperiores
+                aspernatur assumenda distinctio eos, est ex ipsa laboriosam minima nam neque nobis, perferendis
+                perspiciatis porro provident quaerat quas quisquam rerum, sed vero? Accusamus ipsam ipsum, laudantium
+                necessitatibus officiis rem voluptate?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut consequuntur earum facere fuga
+                harum, illum labore, maiores minima nam, nihil non obcaecati odio omnis quaerat quibusdam quis ratione
+                recusandae sed tenetur unde voluptate voluptates. Aliquam aliquid asperiores atque aut consequatur
+                debitis dolore id iste minima odit perspiciatis porro quae, quaerat soluta, tempore temporibus
+                voluptatem? Labore odio placeat reiciendis voluptates? Animi assumenda at deserunt dolores doloribus
+                exercitationem facilis fugiat illum iste minus nisi numquam, optio pariatur quos repudiandae sequi ut,
+                vero! Adipisci amet, beatae iusto mollitia nihil provident totam voluptatem! A, animi architecto
+                consequatur consequuntur dicta distinctio dolorem doloremque dolores ducimus eius expedita facilis
+                ipsum, iste maxime neque omnis porro possimus quaerat quidem reprehenderit repudiandae, sed sint unde
+                vel veniam. Amet asperiores excepturi fuga nisi nulla omnis quae sapiente tempore velit vero. Aliquam
+                delectus dignissimos doloremque incidunt non porro quos sunt voluptatum. Aliquam cupiditate dolores
+                doloribus iste labore molestias necessitatibus officia quisquam rerum, unde? Aliquid aperiam cupiditate
+                facilis incidunt placeat praesentium quas repudiandae sed! Amet aperiam, autem commodi culpa dolores
+                ducimus ea eaque est et eum exercitationem facilis fuga fugiat inventore itaque laborum nemo non nulla
+                omnis optio quaerat reiciendis rem reprehenderit sit veritatis vero vitae! Autem earum eius, harum
+                libero nisi non numquam, quasi quos rem repellendus rerum voluptates! Amet consequatur cumque delectus,
+                deserunt doloremque dolores eum exercitationem expedita facere fugit illum ipsum iure maxime natus
+                necessitatibus, perferendis praesentium quae quidem quis, quo repellat repudiandae temporibus totam
+                voluptatem voluptates. A aliquam aspernatur aut consequatur dignissimos dolores est expedita harum id
+                impedit omnis quis quisquam, ratione rem saepe sint vitae? Amet aspernatur cumque dolor eligendi
+                expedita, iste minima natus nihil, numquam quod rem suscipit unde voluptas! Autem, beatae exercitationem
+                explicabo illo magni quis velit vero. Blanditiis facilis molestias numquam officiis quisquam recusandae
+                reprehenderit sed, voluptatum? Autem ducimus libero magnam unde.
+            </p>
+        </div>
+    );
+}
+
+export default About;
